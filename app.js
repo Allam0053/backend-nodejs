eadc3588e0db1bddb3b97ae1354c63785bc77fb1
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2023-03-24 17:18:00 +0700

    feat: handle request with body POST method

--- app.js
+++ app.js
@@ -1,73 +1,47 @@
 import fs from "fs";
 import path from "path";
+import http from "http";
 
-export default function WritableStreamDemo() {
-  const writableStream = fs.createWriteStream("./filesystem/output.txt");
+export default function app() {
+  const requestListener = (request, response) => {
+    response.setHeader("Content-Type", "text/html");
+    response.statusCode = 200;
 
-  writableStream.write("Ini merupakan teks baris pertama!\n");
-  writableStream.write("Ini merupakan teks baris kedua!\n");
-  writableStream.end("Akhir dari writable stream!");
+    const { method } = request;
+
+    if (method === "GET") {
+      response.end("<h1>Hello!</h1>");
+    }
+
+    if (method === "POST") {
+      let body = [];
+
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Hai, ${name}!</h1>`);
+      });
+    }
+
+    if (method === "PUT") {
+      response.end("<h1>Bonjour!</h1>");
+    }
+
+    if (method === "DELETE") {
+      response.end("<h1>Salam!</h1>");
+    }
+  };
+
+  const server = http.createServer(requestListener);
+
+  const port = 5000;
+  const host = "localhost";
+
+  server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+  });
 }
-
-// export default function ReadableStreamDemo() {
-//   const readableStream = fs.createReadStream("./filesystem/article.txt", {
-//     highWaterMark: 10,
-//   });
-
-//   readableStream.on("readable", () => {
-//     try {
-//       process.stdout.write(`[${readableStream.read()}]`);
-//     } catch (error) {
-//       // catch the error when the chunk cannot be read.
-//       console.log("{{ error }}");
-//     }
-//   });
-
-//   readableStream.on("end", () => {
-//     console.log("Done");
-//   });
-// }
-
-// export default function app() {
-//   const fileReadCallback = (error, data) => {
-//     if (error) {
-//       console.log("Gagal membaca berkas");
-//       return;
-//     }
-//     console.log(data);
-//   };
-
-//   const readFiles = fs.readdirSync("filesystem");
-
-//   /** to be fixed */
-//   const allSources = readFiles.reduce((allSource, val) => {
-//     const data = fs.readFileSync(`filesystem/${val}`, "UTF-8");
-//     return [...allSource, data];
-//   }, []);
-//   /** to be fixed */
-
-//   allSources.forEach((val) => console.log(val, "\n"));
-// }
-
-// export default function nowTo2030() {
-//   const today = new Date();
-//   const newYear = new Date("2030-01-01");
-
-//   const timeDiff = newYear.getTime() - today.getTime();
-//   const daysDiff = timeDiff / (1000 * 60 * 60 * 24);
-
-//   console.log(
-//     `There are ${Math.floor(daysDiff)} days until January 1st, 2030.`
-//   );
-// }
-
-// export default function featureTest() {
-//   console.log(path.resolve("filesystem", "notes.txt"));
-// }
-
-// console.log(fs.readdirSync("filesystem"));
-
-// fs.readFile("todo.txt", "UTF-8", fileReadCallback);
-
-// const data = fs.readFileSync("todo.txt", "UTF-8");
-// console.log(data);
